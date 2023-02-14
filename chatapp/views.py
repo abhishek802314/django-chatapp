7eff653bd43cead96f64cd2ad694881b3e4b3546
Abhi <abhishek802314@gmail.com>
2023-02-14 21:54:35 +0530

    first commit

---
+++ chatapp/views.py
@@ -0,0 +1,16 @@
+from django.shortcuts import render
+from .models import ChatRoom, ChatMessage
+# Create your views here.
+
+def index(request):
+    chatrooms = ChatRoom.objects.all()
+    context = {'chatrooms':chatrooms}
+    return render(request, 'chatapp/index.html', context)
+
+
+def chatroom(request, slug):
+    chatroom = ChatRoom.objects.get(slug=slug)
+    messages = ChatMessage.objects.filter(room=chatroom)[0:30]
+
+    context = {'chatroom':chatroom, 'messages':messages}
+    return render(request, 'chatapp/room.html', context)
